ae12be2b71f40c656b255b9afd503317a23c1934
Sanket Kumar <sanketcharanpahadi@gmail.com>
2022-07-12 16:10:20 +0530

    updated components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./components/Form";
+import GetStarted from "./components/GetStarted";
+import NavBar from "./components/NavBar";
+import Starter from "./components/Starter";
+import Statistics from "./components/Statistics";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <main>
+        <Starter />
+        <section className="form bg-neutral-gray relative pt-40 md:pb-24">
+          <Form />
+          <Statistics />
+        </section>
+      </main>
+      <GetStarted />
+      <Footer />
     </div>
   );
 }
